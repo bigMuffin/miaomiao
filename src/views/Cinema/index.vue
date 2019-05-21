4dfac1cdad65171107533aed8bebff1dcb047fb9
LiuFC <442443285@qq.com>
2019-05-21 11:24:10 +0800

    createComponent

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>hello,Cinema</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>hello,Mine</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
